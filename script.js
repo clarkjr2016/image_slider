7cfb7fa2f78c654c108c2ace255b4b05d3f254a3
Eugene Clark <clarkjr2016@gmail.com>
2023-08-21 20:53:39 -0400

    added functionality to move through the slideshow

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
 const slides = document.querySelectorAll(".slide"); //selects the images so they can be looped through
-
+const rightBtn = document.querySelector("#right"); // this selects the right button
+const leftBtn = document.querySelector("#left");
 let counter = 0;
 
 slides.forEach((slide, index) => {
   slide.style.left = `${index * 100}%`;
-}); //this moves all the images to the right of one another in the order that they were created in.
+}); //this moves all the images to the right of one another in the order that they were created in (based on their index i.e 1 = 100%). The contaainer has overflow hidden on it so the pictures to the right won't be seen until we transition over to them.
 
-function slideImage() {}
+function slideImageForward() {
+  if (counter == 0) {
+    // setting this if statement prevents the slider from going beyond the area with no images
+    return;
+  } else {
+    // this else statement executes the logic that allows the slider to operate
+    counter += 1; // this counter is used to increment the multiplier of the translateX property of the images
+    slides.forEach((slide) => {
+      slide.style.transform = `translateX(${counter * 100}%)`;
+      console.log(counter);
+    });
+  }
+}
+
+function slideImageBackward() {
+  if (counter == -4) {
+    // setting this if statement prevents the slider from going beyond the area with no images
+    return;
+  } else {
+    // this else statement executes the logic that allows the slider to operate
+    counter -= 1; // this counter is used to decrement the multiplier of the translateX property of the images
+    slides.forEach((slide) => {
+      slide.style.transform = `translateX(${counter * 100}%)`;
+      console.log(counter);
+    });
+  }
+}
+
+rightBtn.addEventListener("click", slideImageForward);
+leftBtn.addEventListener("click", slideImageBackward);
