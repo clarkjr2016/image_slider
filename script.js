364bdf20563abaef8cced243a7d827b5b6c0ad05
Eugene Clark <clarkjr2016@gmail.com>
2023-08-24 13:25:06 -0400

    I got the circles to link with a picture. It is not worth the time to figure out how to put in a slider

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ circles.forEach((circle, circle_index) => {
     console.log(counter);
     slides.forEach((slide, slideIndex) => {
       if (slideIndex === circle_index) {
-        slide.style.transform = `translateX(0)`;
+        slide.style.transform = `translateX(${-circle_index * 100}%)`; //SOMEHOW SOMEWAY I GOT THIS TO WORK BUT I NEED TO REVIEW IT MORE TO TRULY UNDERSTAND
       } else {
         slide.style.transform = `translateX(${
           (slideIndex - circle_index) * 100
@@ -27,6 +27,7 @@ circles.forEach((circle, circle_index) => {
     });
   });
 }); // this loops through the list of circles again and clears out the active class on any of the other cirlces before the clicked circle gets it applied
+// this function is also responsible for when a circle is clicked that the appropriate picture appears as well.
 
 function slideImageForward() {
   if (counter == -4) {
@@ -36,7 +37,7 @@ function slideImageForward() {
     // this else statement executes the logic that allows the slider to operate
     counter -= 1; // this counter is used to decrement the multiplier of the translateX property of the images
     slides.forEach((slide) => {
-      slide.style.transform = `translateX(${counter * 100}%)`;
+      slide.style.transform = `translateX(${counter * 100}%)`; // since the counter is getting decremented it will end up being a negativ number. This will then turn the translateX number to a negative which will move the respective slides to the left
     });
     circles.forEach((circle) => {
       circle.classList.remove("active"); // this removes the active class from all circles before it is applied to the selected circle
