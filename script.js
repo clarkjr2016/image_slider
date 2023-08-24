e91aaee9ded0adceb21f7b25336b9c7b34b51817
Eugene Clark <clarkjr2016@gmail.com>
2023-08-23 20:18:56 -0400

    Got the circle functionality to work somewhat, still need to work out  the bug of certain pictures being skipped.

--- script.js
+++ script.js
@@ -2,20 +2,31 @@ const slides = document.querySelectorAll(".slide"); //selects the images so they
 const rightBtn = document.querySelector("#right"); // this selects the right button
 const leftBtn = document.querySelector("#left"); // this selects the right button
 const circles = document.querySelectorAll(".circle"); //this selects all of the circle elements
-let counter = 0;
-
-circles.forEach((circle) => {
-  circle.addEventListener("click", (e) => {
-    circles.forEach((circle) => {
-      circle.classList.remove("active");
-    });
-    circle.classList.add("active");
-  });
-});
+let counter = 0; // this is used to either increase or decrease the translateX multiplier
 
 slides.forEach((slide, index) => {
   slide.style.left = `${index * 100}%`;
 }); //this moves all the images to the right of one another in the order that they were created in (based on their index i.e 1 = 100%). The contaainer has overflow hidden on it so the pictures to the right won't be seen until we transition over to them.
+
+circles.forEach((circle, circle_index) => {
+  circle.addEventListener("click", () => {
+    circles.forEach((circle) => {
+      circle.classList.remove("active"); // this removes the active class from all circles before it is applied to the selected circle
+    });
+    circle.classList.add("active");
+    counter = -circle_index;
+    console.log(counter);
+    slides.forEach((slide, slideIndex) => {
+      if (slideIndex === circle_index) {
+        slide.style.transform = `translateX(0)`;
+      } else {
+        slide.style.transform = `translateX(${
+          (slideIndex - circle_index) * 100
+        }%)`;
+      }
+    });
+  });
+}); // this loops through the list of circles again and clears out the active class on any of the other cirlces before the clicked circle gets it applied
 
 function slideImageForward() {
   if (counter == -4) {
@@ -26,10 +37,15 @@ function slideImageForward() {
     counter -= 1; // this counter is used to decrement the multiplier of the translateX property of the images
     slides.forEach((slide) => {
       slide.style.transform = `translateX(${counter * 100}%)`;
-      console.log(counter);
     });
+    circles.forEach((circle) => {
+      circle.classList.remove("active"); // this removes the active class from all circles before it is applied to the selected circle
+    });
+
+    const circleIndex = -counter - 0;
+    circles[circleIndex].classList.add("active"); // this sets the appropriate circle to be filled with the active class
   }
-}
+} // this function slides the images forward
 
 function slideImageBackward() {
   if (counter == 0) {
@@ -41,10 +57,14 @@ function slideImageBackward() {
     slides.forEach((slide) => {
       slide.classList.toggle("active");
       slide.style.transform = `translateX(${counter * 100}%)`;
-      console.log(counter);
     });
+    circles.forEach((circle) => {
+      circle.classList.remove("active"); // this removes the active class from all circles before it is applied to the selected circle
+    });
+    const circleIndex = -counter - 0;
+    circles[circleIndex].classList.add("active"); // this sets the appropriate circle to be filled with the active class
   }
-}
+} // this function slides the images backwards
 
 rightBtn.addEventListener("click", slideImageForward);
 leftBtn.addEventListener("click", slideImageBackward);
